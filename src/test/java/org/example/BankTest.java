d351b953cbd445652f22b08dd29759bcc33f4c45
Tristan Chaput <tristan.chaput@arolla.fr>
2023-02-04 22:55:53 +0100

    update: refactoring tests

--- src/test/java/org/example/BankTest.java
+++ src/test/java/org/example/BankTest.java
@@ -1,35 +1,21 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BankTest {
 
-    @Test
-    void test_should_return_500_when_deposit_500_on_the_new_account(){
-        Account account = new Account(0);
+    @ParameterizedTest
+    @CsvSource({"500, 0, 500", "100, 0, 100", "200, 100, 100"})
+    void test_should_return_actualized_balance_when_deposit_amount_on_an_account_with_an_initial_amount
+            (int expected, int initialAmount, int amount){
+        Account account = new Account(initialAmount);
 
-        account.deposit(500);
+        account.deposit(amount);
 
-        assertEquals(500, account.balance);
-    }
-
-    @Test
-    void test_should_return_100_when_deposit_100_on_the_new_account(){
-        Account account = new Account(0);
-
-        account.deposit(100);
-
-        assertEquals(100, account.balance);
-    }
-
-    @Test
-    void test_should_return_200_when_deposit_100_on_the_new_account_with_100_at_initial_amount(){
-        Account account = new Account(100);
-
-        account.deposit(100);
-
-        assertEquals(200, account.balance);
+        assertEquals(expected, account.balance);
     }
 }
