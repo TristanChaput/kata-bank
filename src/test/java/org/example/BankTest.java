604a638d0c900d581595ba2c84da0f104d8b0bc3
Tristan Chaput <tristan.chaput@arolla.fr>
2023-02-04 23:03:27 +0100

    update: add a name to test

--- src/test/java/org/example/BankTest.java
+++ src/test/java/org/example/BankTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BankTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "When initial amount is {1}, and deposit amount is {2} the balance should be {0}")
     @CsvSource({"500, 0, 500", "100, 0, 100", "200, 100, 100"})
     void test_should_return_actualized_balance_when_deposit_amount_on_an_account_with_an_initial_amount
             (int expected, int initialAmount, int amount){
