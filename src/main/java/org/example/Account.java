eb3ebb2313b569e38147963c699149efcf67320f
Tristan Chaput <tristan.chaput@arolla.fr>
2023-02-05 11:39:59 +0100

    feat: account has a list of operations, the deposit logic is adapted for operations

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,15 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
 
     private int balance;
+    private final List<Operation> operations;
 
     public Account(int initialAmount) {
         balance = initialAmount;
+        operations = new ArrayList<>();
+        deposit(initialAmount);
     }
 
     public void deposit(int amount) {
-        balance += amount;
+        operations.add(new Operation(OperationType.DEPOSIT, amount));
     }
 
     public void withdraw(int amount) {
@@ -21,5 +27,9 @@ public class Account {
 
     public int getBalance() {
         return balance;
+    }
+
+    public List<Operation> getOperations() {
+        return operations;
     }
 }
--- src/test/java/org/example/BankTest.java
+++ src/test/java/org/example/BankTest.java
@@ -10,15 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BankTest {
 
-    @ParameterizedTest(name = "When initial amount is {1}, and deposit amount is {2} the balance should be {0}")
-    @CsvSource({"500, 0, 500", "100, 0, 100", "200, 100, 100"})
-    void test_should_return_actualized_balance_when_deposit_amount_on_an_account_with_an_initial_amount
-            (int expected, int initialAmount, int amount){
+    @ParameterizedTest(name = "When initial amount is {1}, the amount of the operation should be {0}")
+    @CsvSource({"500, 500", "100, 100", "200, 200"})
+    void test_should_return_a_deposit_operation_when_deposit_amount_on_a_new_account(int expected, int initialAmount){
         Account account = new Account(initialAmount);
 
-        account.deposit(amount);
-
-        assertEquals(expected, account.getBalance());
+        assertEquals(expected, account.getOperations().get(account.getOperations().size() - 1).getAmount());
+        assertEquals(LocalDate.now(), account.getOperations().get(account.getOperations().size() - 1).getDate());
+        assertEquals(OperationType.DEPOSIT, account.getOperations().get(account.getOperations().size() - 1).getType());
     }
 
     @ParameterizedTest(name = "When initial amount is {1}, and withdraw amount is {2} the balance should be {0}")
